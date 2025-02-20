8de1417e2398ad7f6d1f7044843747eb87ec079b
colton-gabertan <gabertan.colton@gmail.com>
2025-02-20 05:07:59 -0800

    delete old directory

--- infrastructure/traefik/install-traefik.sh
+++ infrastructure/traefik/install-traefik.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm install traefik traefik/traefik --namespace traefik --create-namespace --values traefik-values.yml
+helm install traefik traefik/traefik --namespace traefik --create-namespace --values ./helm/values.yml
